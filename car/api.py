72926f076353e7548f73e693f9a649919b58003d
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-28 14:09:04 -0600

    modiify the daa strcuture

--- car/api.py
+++ car/api.py
@@ -4,6 +4,11 @@ from .serializers import CarSerializer
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
-    permission_classes = [permissions.AllowAny]
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:  # GET requests
+            permission_classes = [permissions.AllowAny]
+        else:  # POST, PUT, DELETE requests
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
     serializer_class = CarSerializer
     
--- car/models.py
+++ car/models.py
@@ -5,7 +5,7 @@ class Cars(models.Model):
     engine = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    image_url = models.TextField(blank=False)
+    image_url = models.JSONField(default=list, blank=False)
     colors = models.JSONField(default=list, blank=False) 
     versions = models.JSONField(default=list, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
