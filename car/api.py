8ddee94bc9582253afebb1ec528dc5fc1f456908
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-28 16:51:03 -0600

    restricting put pach delete

--- car/api.py
+++ car/api.py
@@ -4,14 +4,15 @@ from .serializers import CarSerializer
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
-    permission_classes = [permissions.AllowAny]
-    """
+    #permission_classes = [permissions.AllowAny]
+    
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:  # GET requests
             permission_classes = [permissions.AllowAny]
         else:  # POST, PUT, DELETE requests
             permission_classes = [permissions.IsAuthenticated]
         return [permission() for permission in permission_classes]
+    """
     """
     serializer_class = CarSerializer
     
