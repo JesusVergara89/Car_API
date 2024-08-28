532f5c24e3917cb2b202e4691607d91e02088a31
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-28 17:04:13 -0600

    add documentation

--- carapi/settings.py
+++ carapi/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'coreapi',
     'car',
 ]
 
@@ -123,3 +124,8 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
